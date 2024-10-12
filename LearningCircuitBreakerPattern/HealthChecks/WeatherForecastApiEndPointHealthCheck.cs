eb0cdf45598c5bed6c0ad94eae9aad2dbdb9f1b0
RamadossElumalai <Ramadossrohit2264@gmail.com>
2024-10-12 11:58:28 +0530

    Health check end point added

---
+++ LearningCircuitBreakerPattern/HealthChecks/WeatherForecastApiEndPointHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using LearningCircuitBreakerPattern.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace LearningCircuitBreakerPattern.HealthChecks
+{
+    public class WeatherForecastApiEndPointHealthCheck : IHealthCheck
+    {
+        private readonly IWeatherService _weatherService;
+
+        public WeatherForecastApiEndPointHealthCheck(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var weatherForecasts = await _weatherService.GetWeatherForecasts();
+                if (weatherForecasts != null && weatherForecasts.Any())
+                {
+                    return HealthCheckResult.Healthy("WeatherForecast service is working.");
+                }
+                return HealthCheckResult.Degraded("WeatherForecast service returned no data.");
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("WeatherForecast service is unavailable.", ex);
+            }
+        }
+    }
+}
--- LearningCircuitBreakerPattern/Program.cs
+++ LearningCircuitBreakerPattern/Program.cs
@@ -1,6 +1,12 @@
+using LearningCircuitBreakerPattern.HealthChecks;
 using LearningCircuitBreakerPattern.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Polly;
 using Polly.Extensions.Http;
+using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Azure.Cosmos;
 
 // Retry policy added when transient error occurs (status codes 5xx, 408), it will retry 2 times after 1st attempt after 2 seconds and 2nd attempt after 4 seconds
 static IAsyncPolicy<HttpResponseMessage> RetryPolicy()
@@ -38,12 +44,56 @@ static IAsyncPolicy<HttpResponseMessage> CircuitBreakerPolicy()
         );
 }
 
+// health check end point result format
+static Task WriteResponse(HttpContext context, HealthReport healthReport)
+{
+    context.Response.ContentType = "application/json; charset=utf-8";
+
+    var options = new JsonWriterOptions { Indented = true };
+
+    using var memoryStream = new MemoryStream();
+    using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+    {
+        jsonWriter.WriteStartObject();
+        jsonWriter.WriteString("status", healthReport.Status.ToString());
+        jsonWriter.WriteStartObject("results");
+
+        foreach (var healthReportEntry in healthReport.Entries)
+        {
+            jsonWriter.WriteStartObject(healthReportEntry.Key);
+            jsonWriter.WriteString("status",
+                healthReportEntry.Value.Status.ToString());
+            jsonWriter.WriteString("description",
+                healthReportEntry.Value.Description);
+            jsonWriter.WriteStartObject("data");
+
+            foreach (var item in healthReportEntry.Value.Data)
+            {
+                jsonWriter.WritePropertyName(item.Key);
+
+                JsonSerializer.Serialize(jsonWriter, item.Value,
+                    item.Value?.GetType() ?? typeof(object));
+            }
+
+            jsonWriter.WriteEndObject();
+            jsonWriter.WriteEndObject();
+        }
+
+        jsonWriter.WriteEndObject();
+        jsonWriter.WriteEndObject();
+    }
+
+    return context.Response.WriteAsync(
+        Encoding.UTF8.GetString(memoryStream.ToArray()));
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// adding retry and circuit breaker policy
 builder.Services.AddHttpClient<IWeatherService, WeatherService>(options =>
 {
     options.BaseAddress = new Uri("https://localhost:7129/weatherforecast");
@@ -51,9 +101,22 @@ builder.Services.AddHttpClient<IWeatherService, WeatherService>(options =>
 .AddPolicyHandler(RetryPolicy())
 .AddPolicyHandler(CircuitBreakerPolicy());
 
+string cosmosDbConnectionString = "AccountEndpoint=https://test.documents.azure.com:443/;AccountKey=123324324234;Database=employee;";
+
+// health check
+builder.Services.AddHealthChecks()
+    // added custom health check for the api
+    .AddCheck<WeatherForecastApiEndPointHealthCheck>("WeatherForcastApi")
+    // added health check for cosmosdb
+    .AddAzureCosmosDB(svc => new CosmosClient(cosmosDbConnectionString), name: "cosmodb");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    ResponseWriter = WriteResponse
+});
 
 app.UseHttpsRedirection();
 
